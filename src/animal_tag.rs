ef66da726edb94c02053e2bbfa648ab11c1a7633
Martin Kauppinen <martin.kauppinen@gmail.com>
2023-10-10 20:17:49 +0200

    Implement tag reading

---
+++ src/animal_tag.rs
@@ -0,0 +1,45 @@
+use heapless::String;
+
+#[repr(C)]
+#[derive(Default, Debug, Clone, Copy, PartialEq)]
+pub struct AnimalTag {
+    head: u8,
+    card_number: [u8; 10],
+    country_id: [u8; 4],
+    data: u8,
+    animal_flag: u8,
+    _reserved: [u8; 4],
+    user_data: [u8; 6],
+    check_sum: [u8; 2],
+    tail: u8,
+}
+
+impl AnimalTag {
+    pub fn card_number(&self) -> String<10> {
+        let mut buf = String::<10>::new();
+
+        for hex_num in self.card_number.iter().rev() {
+            let _ = buf.push(*hex_num as char);
+        }
+
+        buf
+    }
+}
+
+impl From<[u8; 30]> for AnimalTag {
+    fn from(value: [u8; 30]) -> Self {
+        let mut tag = AnimalTag::default();
+
+        tag.head = value[0];
+        tag.card_number.copy_from_slice(&value[1..11]);
+        tag.country_id.copy_from_slice(&value[11..15]);
+        tag.data = value[15];
+        tag.animal_flag = value[16];
+        tag._reserved.copy_from_slice(&value[17..21]);
+        tag.user_data.copy_from_slice(&value[21..27]);
+        tag.check_sum = [value[27], value[28]];
+        tag.tail = value[29];
+
+        tag
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,45 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+mod animal_tag;
+
 use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0};
+use embassy_rp::peripherals::{DMA_CH0, PIN_23, PIN_25, PIO0, UART1, USB};
 use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::uart::{
+    Async, Config, DataBits, InterruptHandler as UartInterruptHandler, Parity, StopBits, UartRx,
+};
+use embassy_rp::usb::{Driver, InterruptHandler as UsbInterruptHandler};
 use embassy_time::{Duration, Timer};
+use embedded_io_async::Read;
 use static_cell::make_static;
+
+use crate::animal_tag::AnimalTag;
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
 });
 
+bind_interrupts!(struct UartIrqs {
+    UART1_IRQ => UartInterruptHandler<UART1>;
+});
+
+bind_interrupts!(struct UsbIrqs {
+    USBCTRL_IRQ => UsbInterruptHandler<USB>;
+});
+
 #[embassy_executor::task]
 async fn wifi_task(
-    runner: cyw43::Runner<'static, Output<'static, PIN_23>, PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>>,
+    runner: cyw43::Runner<
+        'static,
+        Output<'static, PIN_23>,
+        PioSpi<'static, PIN_25, PIO0, 0, DMA_CH0>,
+    >,
 ) -> ! {
     runner.run().await
 }
@@ -34,21 +55,33 @@ async fn main(spawner: Spawner) {
     let fw = include_bytes!("../embassy/cyw43-firmware/43439A0.bin");
     let clm = include_bytes!("../embassy/cyw43-firmware/43439A0_clm.bin");
 
-    // To make flashing faster for development, you may want to flash the firmwares independently
-    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs download 43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs download 43439A0_clm.bin --format bin --chip RP2040 --base-address 0x10140000
-    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+    let driver = Driver::new(p.USB, UsbIrqs);
+    spawner.spawn(logger_task(driver)).unwrap();
 
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
     let mut pio = Pio::new(p.PIO0, Irqs);
-    let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
 
     let state = make_static!(cyw43::State::new());
     let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
     unwrap!(spawner.spawn(wifi_task(runner)));
+
+    let mut config = Config::default();
+    config.baudrate = 9600;
+    config.data_bits = DataBits::DataBits8;
+    config.parity = Parity::ParityNone;
+    config.stop_bits = StopBits::STOP2;
+    let uart_rx = UartRx::new(p.UART1, p.PIN_5, UartIrqs, p.DMA_CH1, config);
+    unwrap!(spawner.spawn(reader(uart_rx)));
 
     control.init(clm).await;
     control
@@ -57,12 +90,30 @@ async fn main(spawner: Spawner) {
 
     let delay = Duration::from_secs(1);
     loop {
-        info!("led on!");
         control.gpio_set(0, true).await;
         Timer::after(delay).await;
 
-        info!("led off!");
         control.gpio_set(0, false).await;
         Timer::after(delay).await;
     }
+}
+
+#[embassy_executor::task]
+async fn reader(mut rx: UartRx<'static, UART1, Async>) {
+    info!("reading");
+    let delay = Duration::from_secs(1);
+    loop {
+        let mut buf = [0u8; 30];
+        rx.read(&mut buf).await.unwrap();
+        log::info!("RX: {:?}", buf);
+
+        let tag: AnimalTag = unsafe { core::mem::transmute_copy(&buf) };
+        log::info!("Card number: {}", tag.card_number().as_str());
+        Timer::after(delay).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn logger_task(driver: Driver<'static, USB>) {
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
 }
